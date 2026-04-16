42d5f6268296080e91bbf2be1ac2c2cb19ebf937
Tiffany <274891081+tiff-m-tech@users.noreply.github.com>
2026-04-16 10:41:43 -0400

    Completed assignment - added to story.

--- boolean-expressions-and-conditional-statements.js
+++ boolean-expressions-and-conditional-statements.js
@@ -24,23 +24,24 @@ Paste the following code into your editor:
 
 */
 
-const readline = require('readline-sync');
+// original code
+// const readline = require('readline-sync');
 
-const hasTorch = true;
-const hasMap = false;
+// const hasTorch = true;
+// const hasMap = false;
 
-console.log("You see two paths: one leads to the mountains, the other to the village.");
-const choice = readline.question("Do you go to the 'mountains' or the 'village'?");
+// console.log("You see two paths: one leads to the mountains, the other to the village.");
+// const choice = readline.question("Do you go to the 'mountains' or the 'village'?");
 
-if (choice === "mountains" && hasTorch) {
-  console.log("You safely navigate through the dark mountains.");
-} else if (choice === "mountains" && !hasTorch) {
-  console.log("It's too dark to proceed. You decide to turn back.");
-} else if (choice === "village" || hasMap) {
-  console.log("You find your way to the village.");
-} else {
-  console.log("You get lost and wander aimlessly.");
-}
+// if (choice === "mountains" && hasTorch) {
+//   console.log("You safely navigate through the dark mountains.");
+// } else if (choice === "mountains" && !hasTorch) {
+//   console.log("It's too dark to proceed. You decide to turn back.");
+// } else if (choice === "village" || hasMap) {
+//   console.log("You find your way to the village.");
+// } else {
+//   console.log("You get lost and wander aimlessly.");
+// }
 
 /* 
 
@@ -50,3 +51,53 @@ Add Customization and expand the game:
   - Use nested conditionals and logical operators to create complex outcomes.
 
 */
+
+const input = require('readline-sync');
+
+const hasTorch = true;
+const hasMap = false;
+let hasMoney = false;
+
+console.log("You see two paths: one leads to the mountains, the other to the village.");
+const choice = input.question("Do you go to the 'mountains' or the 'village'? ");
+
+if (choice === "mountains" && hasTorch) {
+  console.log("You safely navigate through the dark mountains.");
+  console.log("You happen upon a cave.");
+
+  const caveChoice = input.question("Do you 'enter the cave' or 'keep walking'? ");
+
+  if (caveChoice === "enter the cave") {
+    console.log("You enter the cave and use your torch to explore. You stumble upon a treasure chest!");
+  } else if (caveChoice === "keep walking" && hasMap) {
+    console.log("You decide to keep walking and use your map to find your way home. END OF STORY");
+  } else if (caveChoice === "keep walking" && !hasMap) {
+    console.log("You decide to keep walking, but without a map you get lost, wandering the woods the rest of your life. END OF STORY");
+  } else {
+    console.log("That is not a valid choice.");
+  }
+} else if (choice === "mountains" && !hasTorch) {
+  console.log("It's too dark to proceed. You decide to turn back.");
+} else if (choice === "village") {
+  console.log("You find your way to the village.");
+  console.log("While walking around you find a bag.");
+    
+    const villageChoice = input.question("Do you 'pick up the bag' or 'leave the bag'?")
+
+    if (villageChoice === 'pick up the bag') {
+      console.log("YAY, you found some money and keep walking.");
+      hasMoney = true;
+    } else {
+      console.log("You keep walking.");
+    } 
+  
+    console.log("You happen upon a store and decide to enter. They sell food. You decide to buy some!")
+
+    if (hasMoney) {
+      console.log("You buy some food. After a while you start feeling sick and pass out. It was poisoned! END OF GAME");
+    } else {
+      console.log("You realize you don't have money and can't buy anything. Leaving the store disappointed but continuing your journey.");
+    }
+} else {
+  console.log("You get lost and wander aimlessly.");
+}
